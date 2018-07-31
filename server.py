963d61758f629b1658641b887015e77c9d26f38a
eric-maynard <eric.w.maynard@gmail.com>
2018-07-31 16:42:32 -0400

    updated server for single page; qol

--- server.py
+++ server.py
@@ -5,17 +5,18 @@ from Imdb import Imdb
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/home')
-@app.route('/search')
+
 def home():
     return render_template('index.html')
 
 
+@app.route('/')
+@app.route('/home')
+@app.route('/search')
 @app.route('/chart')
 def chart():
     if request.args.get('search') is None:
-        return "Invalid url"
+        search = None
     else:
         search = str(request.args.get('search')).strip()
     
@@ -24,15 +25,18 @@ def chart():
         if str(request.args.get('scale')) == 'True':
             scale = bool(request.args.get('scale'))
 
-    try:
-        true_title, episodes = RatingCharts.getEpisodes(Imdb(), search)
+    if search is not None:
         try:
-            chart_div = RatingCharts.buildChart(true_title, episodes, scale)
+            true_title, episodes = RatingCharts.getEpisodes(Imdb(), search)
+            try:
+                chart_div = RatingCharts.buildChart(true_title, episodes, scale)
+            except:
+                return "Unable to build chart for \"" + search + "\""
         except:
-            return "Unable to build chart for \"" + search + "\""
-    except:
-        return "Unable to scrape episode data for \"" + search + "\""
-    return render_template('chart.html', chart_div = chart_div, title = true_title)
+            return "Unable to scrape episode data for \"" + search + "\""
+        return render_template('chart.html', chart_div = chart_div, title = true_title)
+    else:
+        return render_template('chart.html', chart_div = "", title = "Visualize TV Ratings")
     
 
     
